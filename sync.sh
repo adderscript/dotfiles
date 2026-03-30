2832a1f2301600add35b5dc1a441f363c709fcda
Alasdair Gray <alasdairthgray@gmail.com>
2026-03-30 17:26:20 +0100

    switched terminal to foot

--- sync.sh
+++ sync.sh
@@ -7,7 +7,7 @@ rm -rf ~/dotfiles/.config/*
 rsync -a ~/.config/niri ~/dotfiles/$computer
 rsync -a ~/.config/waybar ~/dotfiles/$computer
 rsync -a ~/.config/nvim ~/dotfiles/$computer
-rsync -a ~/.config/kitty ~/dotfiles/$computer
+rsync -a ~/.config/foot ~/dotfiles/$computer
 rsync -a ~/.config/mako ~/dotfiles/$computer
 rsync -a ~/.config/fuzzel ~/dotfiles/$computer
 rsync -a ~/.config/fastfetch/ ~/dotfiles/$computer/fastfetch
