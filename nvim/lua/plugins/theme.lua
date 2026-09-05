886064fda5f20fbc3265ed6f45deb38ff74598b5
Alasdair Gray <alasdairthgray@gmail.com>
2026-09-05 22:30:19 +0100

    more stuff

--- nvim/lua/plugins/theme.lua
+++ nvim/lua/plugins/theme.lua
@@ -3,8 +3,12 @@ return {
     "xero/miasma.nvim",
     lazy = false,
     priority = 1000,
-    config = function()
-      vim.cmd("colorscheme miasma")
-    end,
-  }
+  },
+
+  {
+    "LazyVim/LazyVim",
+    opts = {
+      colorscheme = "miasma",
+    },
+  },
 }
