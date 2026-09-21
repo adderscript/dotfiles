95872e4be0081d33930a49da38e1649f4e0da4a8
Alasdair Gray <alasdairthgray@gmail.com>
2026-09-21 20:30:07 +0100

    added theme system

--- .config/nvim/lua/plugins/theme.lua
+++ .config/nvim/lua/plugins/theme.lua
@@ -4,7 +4,19 @@ return {
     lazy = false,
     priority = 1000,
   },
-
+  {
+    "xiyaowong/transparent.nvim",
+    opts = {
+      extra_groups = {
+        "NormalFloat",
+        "NeoTreeNormal",
+      },
+    },
+    config = function(_, opts)
+      require("transparent").setup(opts)
+      vim.cmd("TransparentEnable")
+    end,
+  },
   {
     "LazyVim/LazyVim",
     opts = {
